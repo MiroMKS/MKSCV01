0257030477f37bc15e38610671c0fbfd24c56b41
Miroslav Pavlik <miroslavpavlik213@gmail.com>
2021-10-01 12:03:54 +0200

    cv1 a cv2

---
+++ cv02/Debug/Src/system_stm32f0xx.d
@@ -0,0 +1,19 @@
+Src/system_stm32f0xx.o: ../Src/system_stm32f0xx.c \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cm0.h \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cmInstr.h \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cmFunc.h \
+ C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cm0.h:
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cmInstr.h:
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Include/core_cmFunc.h:
+
+C:/MKSCV01/MKSCV01/cv02/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
