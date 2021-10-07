db5f7428ef9de452d8dabe7794ad208d41f27494
Miroslav Pavlik <miroslavpavlik213@gmail.com>
2021-10-07 17:01:37 +0200

    cv3

---
+++ cv3/Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cm0.h \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cmInstr.h \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cmFunc.h \
+ C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ ../Src/sct.h
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cm0.h:
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cmInstr.h:
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Include/core_cmFunc.h:
+
+C:/MKSCV01/MKSCV01/cv3/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+../Src/sct.h:
